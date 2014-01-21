aaaeeb74b09b5368cb4f42c202fd226856c6bd1b
bxshi <baoxu.shi@gmail.com>
2014-01-21 17:19:05 -0500

    add test to compare result by SQL query and in-memory check

--- DiscrMetaPath/src/test/java/edu/nd/dsg/wiki/util/TypeFinderMemTest.java
+++ DiscrMetaPath/src/test/java/edu/nd/dsg/wiki/util/TypeFinderMemTest.java
@@ -9,10 +9,12 @@ import java.util.LinkedHashSet;
 public class TypeFinderMemTest {
 
     private TypeFinderMem typeFinderMem;
+    private TypeFinderSQL typeFinderSQL;
 
     @Before
     public void setUp() throws Exception {
         typeFinderMem = TypeFinderMem.getInstance();
+        typeFinderSQL = TypeFinderSQL.getInstance();
     }
 
     @Test
@@ -23,6 +25,8 @@ public class TypeFinderMemTest {
     @Test
     public void testgetTypeVector() throws Exception {
         LinkedHashSet<Integer> typeVector = typeFinderMem.getTypeVector(12);
+        LinkedHashSet<Integer> typeVectorSQL = typeFinderSQL.getTypeVector(12);
+        Assert.assertArrayEquals(typeVectorSQL.toArray(), typeVector.toArray());
         Assert.assertEquals(6177, typeVector.size());
         typeVector = typeFinderMem.getTypeVector("1921");
         Assert.assertEquals(6399, typeVector.size());
