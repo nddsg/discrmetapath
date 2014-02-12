9f4f9741eaad480e8d3cda2e7ccf76972d8c9d5f
bxshi <baoxu.shi@gmail.com>
2014-02-12 17:48:44 -0500

    generate accumulate distance by bm25

--- DiscrMetaPath/src/main/java/edu/nd/dsg/wiki/BM25Distance.java
+++ DiscrMetaPath/src/main/java/edu/nd/dsg/wiki/BM25Distance.java
@@ -17,15 +17,68 @@ public class BM25Distance {
         CSVWriter writer = new CSVWriter(new FileWriter("./nonorder_bm25.csv"));
         CSVWriter nonWriter = new CSVWriter(new FileWriter("./order_bm25.csv"));
 
-        getResultLines(writer, nonorderPath);
-        getResultLines(nonWriter, orderPath);
+        getSeqResultLines(writer, nonorderPath);
+        getSeqResultLines(nonWriter, orderPath);
+
+        writer.close();
+        nonWriter.close();
+
+        writer = new CSVWriter(new FileWriter("./accumulate_bm25_order.csv"));
+        nonWriter = new CSVWriter(new FileWriter("./accumulate_bm25_non_order.csv"));
+
+        getAccumResultLines(writer, orderPath);
+        getAccumResultLines(nonWriter, nonorderPath);
 
         writer.close();
         nonWriter.close();
 
     }
 
-    protected static void getResultLines(CSVWriter csvWriter, LinkedList<LinkedList<Integer>> pathList){
+    protected static void getAccumResultLines(CSVWriter csvWriter, LinkedList<LinkedList<Integer>> pathList){
+        int groupId = 1;
+        while(pathList.size()>0){
+            LinkedList<LinkedList<Integer>> workList = new LinkedList<LinkedList<Integer>>();
+            LinkedList<String[]> lines = new LinkedList<String[]>();
+            int len = 0;
+            while(len<5){
+                workList.add(pathList.pollFirst());
+                len++;
+            }
+            System.out.println("try calculate groupID:"+groupId+" among paths:"+workList);
+            try{
+                int pathId = 1;
+                for(LinkedList<Integer> path : workList){
+                    LinkedList<Double> accumulateCosine = tfCalculator.getaccumulatedTF(path);
+                    String[] line = new String[2+accumulateCosine.size()];
+                    line[0] = Integer.toString(groupId);
+                    line[1] = Integer.toString(pathId);
+                    for(int pos = 0; pos < accumulateCosine.size(); pos++){
+                        line[pos+2] = Double.toString(accumulateCosine.get(pos));
+                    }
+                    lines.add(line);
+                    pathId++;
+                }
+                if(csvWriter!=null){
+                    csvWriter.writeAll(lines);
+                }else{
+                    for(String[] line : lines){
+                        for(String item : line){
+                            System.out.print(item + " ");
+                        }
+                        System.out.println();
+                    }
+                }
+            }catch (NullPointerException e){
+                //TODO fix redirect page terms now just ignore them...
+                System.out.println("Get non-page stuff or redirect page, ignore it...");
+                e.printStackTrace();
+            }
+            groupId++;
+
+        }
+    }
+
+    protected static void getSeqResultLines(CSVWriter csvWriter, LinkedList<LinkedList<Integer>> pathList){
         int groupId = 1;
         while(pathList.size()>0){
             LinkedList<LinkedList<Integer>> workList = new LinkedList<LinkedList<Integer>>();
