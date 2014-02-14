a7e37e6cb9fbb7d2da9c3b7cbda69e948c6ef4aa
bxshi <baoxu.shi@gmail.com>
2014-02-13 20:36:57 -0500

    reformat output csv

--- DiscrMetaPath/src/main/java/edu/nd/dsg/wiki/PathCosSimilarity.java
+++ DiscrMetaPath/src/main/java/edu/nd/dsg/wiki/PathCosSimilarity.java
@@ -9,14 +9,19 @@ import java.util.HashSet;
 import java.util.LinkedList;
 
 public class PathCosSimilarity {
+    static final String[] header = {"groupId", "pathId", "nodeId", "value"};
 
     static TFSimpleCalculator tfCalculator = TFSimpleCalculator.getInstance();
+
     public static void main(String[] args) throws IOException {
         LinkedList<LinkedList<Integer>> orderPath = new LinkedList<LinkedList<Integer>>();
         LinkedList<LinkedList<Integer>> nonorderPath = new LinkedList<LinkedList<Integer>>();
         txtPathLoader("./data/allpath.txt", true, true, 3, Integer.MAX_VALUE, orderPath, nonorderPath);
-        CSVWriter csvWriterOrder =  new CSVWriter(new FileWriter("./accumulate_cosine_order.csv"));
-        CSVWriter csvWriterNonOrder =  new CSVWriter(new FileWriter("./accumulate_cosine_non_order.csv"));
+        CSVWriter csvWriterOrder = new CSVWriter(new FileWriter("./accumulate_cosine_order.csv"));
+        CSVWriter csvWriterNonOrder = new CSVWriter(new FileWriter("./accumulate_cosine_non_order.csv"));
+
+        csvWriterOrder.writeNext(header);
+        csvWriterNonOrder.writeNext(header);
 
         getResultLines(csvWriterOrder, orderPath);
         getResultLines(csvWriterNonOrder, nonorderPath);
@@ -25,32 +30,34 @@ public class PathCosSimilarity {
         csvWriterNonOrder.close();
     }
 
-    protected static void getResultLines(CSVWriter csvWriter, LinkedList<LinkedList<Integer>> pathList){
+    protected static void getResultLines(CSVWriter csvWriter, LinkedList<LinkedList<Integer>> pathList) {
         int groupId = 1;
-        while(pathList.size()>0){
+        while (pathList.size() > 0) {
             LinkedList<LinkedList<Integer>> workList = new LinkedList<LinkedList<Integer>>();
             LinkedList<String[]> lines = new LinkedList<String[]>();
             int len = 0;
-            while(len<5){
+            while (len < 5) {
                 workList.add(pathList.pollFirst());
                 len++;
             }
-            System.out.println("try calculate groupID:"+groupId+" among paths:"+workList);
-            try{
+            System.out.println("try calculate groupID:" + groupId + " among paths:" + workList);
+            try {
                 int pathId = 1;
-                for(LinkedList<Integer> path : workList){
+                for (LinkedList<Integer> path : workList) {
                     LinkedList<Double> accumulateCosine = tfCalculator.getaccumulatedTF(path);
-                    String[] line = new String[2+accumulateCosine.size()];
-                    line[0] = Integer.toString(groupId);
-                    line[1] = Integer.toString(pathId);
-                    for(int pos = 0; pos < accumulateCosine.size(); pos++){
-                        line[pos+2] = Double.toString(accumulateCosine.get(pos));
+                    for (int nodeId = 0; nodeId < accumulateCosine.size(); nodeId++) {
+                        String[] line = new String[header.length];
+                        line[0] = Integer.toString(groupId);
+                        line[1] = Integer.toString(pathId);
+                        line[2] = Integer.toString(nodeId);
+                        line[3] = Double.toString(accumulateCosine.get(nodeId));
+                        lines.add(line);
+
                     }
-                    lines.add(line);
                     pathId++;
                 }
                 csvWriter.writeAll(lines);
-            }catch (NullPointerException e){
+            } catch (NullPointerException e) {
                 //TODO fix redirect page terms now just ignore them...
                 System.out.println("Get non-page stuff or redirect page, ignore it, ignore it...");
             }
@@ -61,83 +68,83 @@ public class PathCosSimilarity {
 
     protected static void txtPathLoader(String path, boolean retrieveDistinguishPaths,
                                         boolean retrieveOtherPaths, int otherPathNumber, int readLines,
-                                        LinkedList<LinkedList<Integer>> orderPath, LinkedList<LinkedList<Integer>> nonorderPath){
-        try{
+                                        LinkedList<LinkedList<Integer>> orderPath, LinkedList<LinkedList<Integer>> nonorderPath) {
+        try {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(path));
             String line;
             int minSize = 0;
             LinkedList<LinkedList<Integer>> resPathList = null;
             //mark its separator
 
-            if(retrieveDistinguishPaths){
+            if (retrieveDistinguishPaths) {
                 minSize += 2;
             }
-            if(retrieveOtherPaths){
+            if (retrieveOtherPaths) {
                 minSize += otherPathNumber;
             }
 
             line = bufferedReader.readLine();
 
 
-            while(line != null && !line.isEmpty()) {
-                if(line.startsWith("non-order")||line.startsWith("order")){
-                    if(nonorderPath.size()>=readLines && orderPath.size()>=readLines){
+            while (line != null && !line.isEmpty()) {
+                if (line.startsWith("non-order") || line.startsWith("order")) {
+                    if (nonorderPath.size() >= readLines && orderPath.size() >= readLines) {
                         return;
                     }
-                    if(line.startsWith("non-order")){
+                    if (line.startsWith("non-order")) {
                         resPathList = nonorderPath;
-                    }else{
+                    } else {
                         resPathList = orderPath;
                     }
                     int size = Integer.parseInt(line.split(",")[1]);
-                    if(size >= minSize) {
+                    if (size >= minSize) {
                         HashSet<Integer> nodeSet = new HashSet<Integer>();
                         LinkedList<String> pathList = new LinkedList<String>();
                         StringBuilder stringBuilder = new StringBuilder();
-                        while (size > 0){
+                        while (size > 0) {
                             size--;
                             line = bufferedReader.readLine();
-                            pathList.add(line.split("],")[0].replace("[",""));
-                            if(line.split("],")[0].replace("[","").split(",").length<=2){
+                            pathList.add(line.split("],")[0].replace("[", ""));
+                            if (line.split("],")[0].replace("[", "").split(",").length <= 2) {
                                 pathList.pollLast();
                             }
 
                         }
-                        if(pathList.size()>minSize){
-                            String[] strArryLast=null;
-                            if(retrieveDistinguishPaths){
+                        if (pathList.size() > minSize) {
+                            String[] strArryLast = null;
+                            if (retrieveDistinguishPaths) {
                                 LinkedList<Integer> pList = new LinkedList<Integer>();
-                                String[] strArry = pathList.pollFirst().replace(" ","").split(",");
-                                strArryLast = pathList.pollLast().replace(" ","").split(",");
+                                String[] strArry = pathList.pollFirst().replace(" ", "").split(",");
+                                strArryLast = pathList.pollLast().replace(" ", "").split(",");
 
-                                for(String node : strArry) {
+                                for (String node : strArry) {
                                     pList.add(Integer.parseInt(node));
                                 }
                                 resPathList.add(pList);
                             }
-                            if(retrieveOtherPaths){
+                            if (retrieveOtherPaths) {
                                 int cnt = 1;
-                                while(cnt <= otherPathNumber){
+                                while (cnt <= otherPathNumber) {
                                     LinkedList<Integer> pList = new LinkedList<Integer>();
-                                    String[] strArry = pathList.get((pathList.size() - 1) * cnt / otherPathNumber).replace(" ","").split(",");
-                                    for(String node : strArry) {
+                                    String[] strArry = pathList.get((pathList.size() - 1) * cnt / otherPathNumber).replace(" ", "").split(",");
+                                    for (String node : strArry) {
                                         pList.add(Integer.parseInt(node));
                                     }
                                     resPathList.add(pList);
                                     cnt++;
                                 }
                             }
-                            if(retrieveDistinguishPaths){
+                            if (retrieveDistinguishPaths) {
                                 LinkedList<Integer> pList = new LinkedList<Integer>();
-                                for(String node : strArryLast) {
+                                for (String node : strArryLast) {
                                     pList.add(Integer.parseInt(node));
                                 }
                                 resPathList.add(pList);
                             }
                         }
 
-                    }else{
-                        while(size > 0){
+                    } else {
+                        while (size > 0) {
                             bufferedReader.readLine();
                             size--;
                         }
